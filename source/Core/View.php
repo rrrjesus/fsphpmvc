3a6285085e336f1dff0572b2355b97062dfc967c
Rodolfo Romaioli Ribeiro de Jesus <rrrjesus@users.noreply.github.com>
2023-07-19 06:17:00 -0300

    Update inicial

---
+++ source/Core/View.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Source\Core;
+
+use League\Plates\Engine;
+
+
+class View
+{
+    /** @var Engine */
+    private $engine ;
+
+    /**
+     * View constructor.
+     * @param string $path
+     * @param string $ext
+     */
+    public function __construct(string $path = CONF_VIEW_PATH, string $ext = CONF_VIEW_EXT)
+    {
+        $this->engine = new Engine($path, $ext);
+    }
+
+    /**
+     * @param string $name
+     * @param string $path Tem que ser o caminho é pasta
+     * @return View
+     */
+    public function path(string $name, string $path): View
+    {
+        $this->engine->addFolder($name, CONF_VIEW_PATH."/".$path);
+        return $this;
+    }
+
+    /**
+     * @param string $templateName
+     * @param array $data
+     * @return string
+     */
+    public function render(string $templateName, array $data): string
+    {
+        return $this->engine->render($templateName, $data);
+    }
+
+    /**
+     * @return Engine
+     */
+    public function engine(): Engine
+    {
+        return $this->engine();
+    }
+}
